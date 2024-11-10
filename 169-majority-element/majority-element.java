06f20d9ef0d0cb0c39e614f8334bf41611aa7cff
Vaibhav Pal <145701339+vaibhavpal7549@users.noreply.github.com>
2024-11-10 15:36:35 +0530

    Update majority-element.java

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,34 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate = nums[0];
-        int count = 1;
+        int cand = 0;
+        int count = 0;
+        for(int i = 0; i<nums.length; i++){
+            if(count==0){
+                cand = nums[i];
+            }
 
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == candidate) {
+            if(cand == nums[i]){
                 count++;
-            } else {
+            }
+            else{
                 count--;
             }
-
-            if (count == 0) {
-                candidate = nums[i];
-                count = 1;
-            }
-        }
-        
-        // Optional: Verify if the candidate is indeed the majority element
-        int freq = 0;
-        for (int num : nums) {
-            if (num == candidate) {
-                freq++;
-            }
         }
 
-        if (freq > nums.length / 2) {
-            return candidate;
-        }
-
-        // Shouldn't reach here for valid input where a majority element is guaranteed.
-        return -1; 
+        return cand;
     }
 }
