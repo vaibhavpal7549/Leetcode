2a945fba914584056909d9c6d2bc7f1edab97794
Vaibhav Pal <145701339+vaibhavpal7549@users.noreply.github.com>
2025-02-18 23:31:36 +0530

    Time: 8 ms (25.98%) | Memory: 46.5 MB (25.76%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -7,6 +7,9 @@ class Solution {
                 if(nums[i] == nums[j]){
                     count++;
                 }
+                else{
+                    break;
+                }
 
             }
             
