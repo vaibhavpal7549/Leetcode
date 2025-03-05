18c7089afd8f1b543a7877fc06b26eb6957a4d7f
Vaibhav Pal <145701339+vaibhavpal7549@users.noreply.github.com>
2025-03-05 23:55:00 +0530

    Time: 1 ms (99.85%) | Memory: 46.5 MB (25.55%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,40 +1,40 @@
 
-// class Solution {
-//     public int singleNumber(int[] nums) {
-//         int result = 0;
-//         for (int num : nums) {
-//             result ^= num; // XOR all numbers
-//         }
-//         return result;
-//     }
-// }
-
-
-
 class Solution {
     public int singleNumber(int[] nums) {
-        Arrays.sort(nums);
-        for(int i  = 0; i<nums.length; i++){
-            int count = 1;
-            for(int j = i+1; j<nums.length; j++){
-                if(nums[i] == nums[j]){
-                    count++;
-                }
-                else{
-                    break;
-                }
-
-            }
-            
-            if(count == 1){
-                return nums[i];
-            }
-            else{
-                i = i + count-1;
-            }
+        int result = 0;
+        for (int num : nums) {
+            result ^= num; // XOR all numbers
         }
-        return -1;    
+        return result;
     }
-    
 }
+
+
+
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         Arrays.sort(nums);
+//         for(int i  = 0; i<nums.length; i++){
+//             int count = 1;
+//             for(int j = i+1; j<nums.length; j++){
+//                 if(nums[i] == nums[j]){
+//                     count++;
+//                 }
+//                 else{
+//                     break;
+//                 }
+
+//             }
+            
+//             if(count == 1){
+//                 return nums[i];
+//             }
+//             else{
+//                 i = i + count-1;
+//             }
+//         }
+//         return -1;    
+//     }
+    
+// }
 
