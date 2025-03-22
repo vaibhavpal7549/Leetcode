5d3404f6b9e28d736a2a77ee8a142da1f4ea4c7b
Vaibhav Pal <145701339+vaibhavpal7549@users.noreply.github.com>
2025-03-22 12:36:50 +0530

    Time: 20 ms (15.56%) | Memory: 44.3 MB (15.51%) - LeetSync

--- 415-add-strings/add-strings.java
+++ 415-add-strings/add-strings.java
@@ -1,20 +1,28 @@
+import java.math.BigInteger;
 class Solution {
     public String addStrings(String num1, String num2) {
-        StringBuilder result = new StringBuilder();
-        int i = num1.length() - 1, j = num2.length() - 1, carry = 0;
+        // StringBuilder result = new StringBuilder();
+        // int i = num1.length() - 1, j = num2.length() - 1, carry = 0;
 
-        while (i >= 0 || j >= 0 || carry > 0) {
-            int digit1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
-            int digit2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
-            int sum = digit1 + digit2 + carry;
+        // while (i >= 0 || j >= 0 || carry > 0) {
+        //     int digit1 = (i >= 0) ? num1.charAt(i) - '0' : 0;
+        //     int digit2 = (j >= 0) ? num2.charAt(j) - '0' : 0;
+        //     int sum = digit1 + digit2 + carry;
             
-            result.append(sum % 10);
-            carry = sum / 10;
+        //     result.append(sum % 10);
+        //     carry = sum / 10;
             
-            i--;
-            j--;
-        }
+        //     i--;
+        //     j--;
+        // }
         
-        return result.reverse().toString();
+        // return result.reverse().toString();
+
+        BigInteger n1 = new BigInteger(num1);
+        BigInteger n2 = new BigInteger(num2);
+
+        BigInteger ans = n1.add(n2);
+        return ans.toString();
+
     }
 }
