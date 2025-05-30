ce69e4da462ec3dd4b4aa3174a59bc91e7b002ad
Vaibhav Pal <145701339+vaibhavpal7549@users.noreply.github.com>
2025-05-30 11:55:28 +0530

    Time: 1 ms (84.65%) | Memory: 40.7 MB (89.60%) - LeetSync

--- 3394-minimum-array-end/minimum-array-end.java
+++ 3394-minimum-array-end/minimum-array-end.java
@@ -1,10 +1,31 @@
-class Solution {
+// class Solution {
+//     public long minEnd(int n, int x) {
+//         long res = x;
+//         for(int i = 0; i< n-1; i++){
+//             res = (res + 1) | x;
+//         }
+//         return res;
+        
+//     }
+// }
+
+
+public class Solution {
     public long minEnd(int n, int x) {
         long res = x;
-        for(int i = 0; i< n-1; i++){
-            res = (res + 1) | x;
+        long i_x = 1;
+        long i_n = 1; // for n - 1
+
+        while (i_n <= n - 1) {
+            if ((i_x & x) == 0) {
+                if ((i_n & (n - 1)) != 0) {
+                    res = res | i_x;
+                }
+                i_n = i_n << 1;
+            }
+            i_x = i_x << 1;
         }
+
         return res;
-        
     }
 }
