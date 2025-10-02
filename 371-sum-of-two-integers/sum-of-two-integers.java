fffb70a11a9629048d4e4090b0f97739cdbb1646
Vaibhav Pal <145701339+vaibhavpal7549@users.noreply.github.com>
2025-10-02 22:09:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (81.08%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.java
+++ 371-sum-of-two-integers/sum-of-two-integers.java
@@ -1,5 +1,12 @@
 class Solution {
     public int getSum(int a, int b) {
-        return a+b;
+        while(b!=0){
+            int sum = a ^b;
+            int carry = (a&b)<<1;
+            a = sum;
+            b = carry;
+
+        }
+        return a;
     }
 }
