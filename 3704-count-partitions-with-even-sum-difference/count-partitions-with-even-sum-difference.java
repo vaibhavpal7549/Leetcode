4d5aea85f100df8fd7176c3f4d75fef65a242270
Vaibhav Pal <145701339+vaibhavpal7549@users.noreply.github.com>
2025-12-05 15:18:54 +0530

    Time: 1 ms (48.14%) | Memory: 43.5 MB (31.42%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.java
@@ -1,32 +1,25 @@
 class Solution {
-
-    public static int sum(int i, int j, int[] nums){
-        int ans = 0;
-        for(int a = i; a<=j; a++){
-            ans += nums[a];
-        }
-        return ans;
-    }
-    public static boolean foundEven(int a, int i, int j, int l , int [] nums){
-
-        int a1sum = 0;
-        int a2sum = 0;
-        a1sum = sum(a, i, nums);
-        a2sum = sum(j, l, nums);
-        int diff = a1sum - a2sum;
-        if(Math.abs(diff)%2 ==0){
-            return true;
-        }
-        return false;
-    }
     public int countPartitions(int[] nums) {
+
         int n = nums.length;
+        int totalSum = 0;
+
+        for(int x : nums) totalSum += x;
+
+        int prefix = 0;
         int count = 0;
-        for(int i = 0; i<n-1;i++){
-            boolean ans = foundEven(0, i, i+1, n-1, nums);
-            if(ans) count++; 
+
+        for(int i = 0; i < n - 1; i++){
+            prefix += nums[i];
+            
+            // right sum = total - prefix
+            int right = totalSum - prefix;
+
+            // check even-difference
+            if((prefix % 2) == (right % 2)){
+                count++;
+            }
         }
         return count;
-
     }
 }
