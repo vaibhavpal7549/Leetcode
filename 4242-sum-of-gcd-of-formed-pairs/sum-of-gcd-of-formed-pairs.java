a7dc73d0d72b456506bbf419e2bee76a6f321678
Vaibhav Pal <145701339+vaibhavpal7549@users.noreply.github.com>
2026-07-16 11:22:53 +0530

    Time: 60 ms (32.50%) | Memory: 126.2 MB (10.50%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.java
@@ -1,27 +1,22 @@
 class Solution {
 
-    public static int gcd(int a, int b){
+    public static long gcd(long a, long b){
         
         while(b!=0){
-            int temp = b;
-            b= a%b;
+            long temp = b;
+            b = a%b;
             a = temp;
         }
         return a;
     }
     public long gcdSum(int[] nums) {
         int n = nums.length;
-        int pgcd[] = new int[n];
+        long pgcd[] = new long[n];
 
-        int mxi[] = new int[n];
         int max = 0;
-        for(int i = 0; i<n; i++){
-            max = Math.max(max, nums[i]);
-            mxi[i] = max;
-        }
-
         for(int i = 0; i<n;i++){
-            pgcd[i] = gcd(nums[i], mxi[i]);
+            max = Math.max(max, nums[i]);
+            pgcd[i] = gcd((long)nums[i], (long)max);
         }
 
         Arrays.sort(pgcd);
@@ -30,10 +25,12 @@ class Solution {
         int l = 0;
         int r = n-1;
         while(l<r){
-            sum += gcd(pgcd[l], pgcd[r]);
-            l++;
-            r--;
+           sum += gcd(pgcd[l], pgcd[r]);
+
+           l++;
+           r--;
         }
+
         return sum;
 
 
