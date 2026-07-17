323d565444cf76a431e7fd1220cb30b72a8d5110
Vaibhav Pal <145701339+vaibhavpal7549@users.noreply.github.com>
2026-07-18 00:30:45 +0530

    Time: 78 ms (74.41%) | Memory: 134.5 MB (27.94%) - LeetSync

--- 307-range-sum-query-mutable/range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/range-sum-query-mutable.java
@@ -1,90 +1,53 @@
-class Node {
-    int start, end;
-    int sum;
-    Node left, right;
+public class NumArray {
+    int[] tree;
+    int[] nums;
+    int n;
 
-    Node(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-}
-
-class SegmentTree {
-    Node root;
-
-    SegmentTree(int[] nums) {
-        if (nums.length > 0) {
-            root = build(nums, 0, nums.length - 1);
-        }
-    }
-
-    private Node build(int[] nums, int start, int end) {
-        Node node = new Node(start, end);
-
-        if (start == end) {
-            node.sum = nums[start];
-            return node;
-        }
-
-        int mid = start + (end - start) / 2;
-
-        node.left = build(nums, start, mid);
-        node.right = build(nums, mid + 1, end);
-
-        node.sum = node.left.sum + node.right.sum;
-
-        return node;
-    }
-
-    int query(Node node, int ql, int qr) {
-        if (node == null)
-            return 0;
-
-        // No overlap
-        if (node.end < ql || node.start > qr)
-            return 0;
-
-        // Complete overlap
-        if (node.start >= ql && node.end <= qr)
-            return node.sum;
-
-        // Partial overlap
-        return query(node.left, ql, qr) +
-               query(node.right, ql, qr);
-    }
-
-    int update(Node node, int idx, int value) {
-        if (node == null)
-            return 0;
-
-        if (idx < node.start || idx > node.end)
-            return node.sum;
-
-        if (node.start == node.end) {
-            node.sum = value;
-            return node.sum;
-        }
-
-        node.sum = update(node.left, idx, value) +
-                   update(node.right, idx, value);
-
-        return node.sum;
-    }
-}
-
-class NumArray {
-
-    SegmentTree tree;
 
     public NumArray(int[] nums) {
-        tree = new SegmentTree(nums);
+        this.n = nums.length;
+        this.nums = new int[n];
+        this.tree = new int[n + 1];
+
+
+        for(int i = 0; i < n; i++)
+            update(i, nums[i]);
     }
 
-    public void update(int index, int val) {
-        tree.update(tree.root, index, val);
+
+    public void update(int index, int val){
+        int delta = val - nums[index];
+        nums[index] += delta;
+
+
+        index++;
+        while(index <= n){
+            tree[index] += delta;
+            index += index & (-index);
+        }
     }
 
-    public int sumRange(int left, int right) {
-        return tree.query(tree.root, left, right);
+
+    private int query(int index){
+        int sum = 0;
+        index++;
+        while(index > 0){
+            sum += tree[index];
+            index -= index & (-index);
+        }
+        return sum;
     }
+
+
+    public int sumRange(int left, int right){
+        return query(right) - query(left - 1);
+    }  
 }
+
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(index,val);
+ * int param_2 = obj.sumRange(left,right);
+ */
