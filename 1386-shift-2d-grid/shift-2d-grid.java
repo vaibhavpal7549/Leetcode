e0727d44f09b5f7bca67fbab1329880b22ef427b
Vaibhav Pal <145701339+vaibhavpal7549@users.noreply.github.com>
2026-07-20 11:31:23 +0530

    Time: 4 ms (97.01%) | Memory: 46.9 MB (96.55%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.java
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -1,40 +1,33 @@
 class Solution {
-    public List<List<Integer>> shiftGrid(int[][] arr, int k) {
-        
-        int m = arr.length;
-        int n = arr[0].length;
-        while(k-->0){
-            int prev = arr[m-1][n-1];
-            int curr = arr[0][0];
-
-            for(int i = 0;i<m;i++){
-                for(int j = 0; j<n; j++){
-                    if(i == 0 && j == 0){
-                        arr[i] [ j] = prev;
-                    }else{
-                        curr = arr[i][j];
-                        arr[i][j] = prev;
-                    }
-                    prev = curr;
-                }
-            }
-
-
-        }
+    public List<List<Integer>> shiftGrid(int[][] grid, int k) {
         List<List<Integer>> ans = new ArrayList<>();
+        int m = grid.length;
+        int n = grid[0].length;
 
-        
-            
+        int [] temp = new int[m*n];
+        int t = temp.length;
+        int idx = 0;
 
-            for(int i = 0; i<m; i++){
-                List<Integer> al = new ArrayList<>();
-                for(int j = 0; j <n; j++){
-                    al.add(arr[i][j]);
-                }
-                ans.add(al);
-
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                temp[idx++] = grid[i][j];
             }
-            return ans;
-    
+        }
+
+        k %= t;
+        int move = (t - k) % t;
+
+        int idx1 = move;
+        for(int i = 0 ;i<m ;i++){
+            List<Integer> al = new ArrayList<>();
+            for(int j = 0; j<n; j++){
+               al.add(temp[idx1]);
+                idx1 = (idx1 + 1) % t;
+            }
+            ans.add(al);
+        }
+
+        return ans;
+        
     }
 }
