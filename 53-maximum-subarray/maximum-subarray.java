b443042ba5b4d47e9cd60369555db9210c35b92d
Vaibhav Pal <145701339+vaibhavpal7549@users.noreply.github.com>
2026-08-10 07:46:12 +0530

    Time: 1 ms (99.96%) | Memory: 77.2 MB (66.89%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -5,11 +5,12 @@ class Solution {
         int max = Integer.MIN_VALUE;
 
         for(int i =0;i<n; i++ ){
+            cs += nums[i];
+            max= Math.max(cs, max);
             if(cs < 0){
                 cs = 0;
             }
-            cs += nums[i];
-            max= Math.max(cs, max);
+            
         }
         return max;
     }
