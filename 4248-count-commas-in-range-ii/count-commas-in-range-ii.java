50ce9cece61e1539f4493c11889689834fd5a149
Vaibhav Pal <145701339+vaibhavpal7549@users.noreply.github.com>
2026-09-10 20:40:17 +0530

    Time: 1 ms (99.24%) | Memory: 42.6 MB (69.96%) - LeetSync

--- 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -1,11 +1,18 @@
 class Solution {
     public long countCommas(long n) {
-        long start = 1000;
+        long lower = 1000;
         long res = 0;
+        long comma = 1;
+        long upper = 0;
 
-        while(start <= n){
-            res += n - start + 1;
-            start *= 1000;
+        while(lower <= n){
+            upper = lower * 1000 - 1;
+            if(upper > n){
+                upper = n;
+            }
+            res += (upper-lower + 1) * comma;
+            lower *= 1000;
+            comma += 1;
 
         }
         return res;
